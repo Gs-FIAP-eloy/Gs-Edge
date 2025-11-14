c4758442e0f76ba916c8605425a8ad1ae8559031
Lucas Toledo <lucastoledocort25@gmail.com>
2025-11-14 00:04:18 -0300

    simulador da pulseira em python

---
+++ mqtt_test.py
@@ -0,0 +1,46 @@
+import paho.mqtt.client as mqtt
+import json
+import time
+import random
+
+BROKER = "broker.emqx.io"
+PORT = 1883
+
+TOPIC_DATA = "eloy/band01/data"
+TOPIC_ALERTS = "eloy/band01/alerts"
+
+client = mqtt.Client()
+client.connect(BROKER, PORT, 60)
+client.loop_start()
+
+def calcular_modo(hr, dist):
+    if hr == 0:
+        return "WorkOFF"
+    if hr > 0 and dist > 50:
+        return "WorkON"
+    if hr > 0 and dist <= 50:
+        return "Working"
+
+while True:
+    # Simulação
+    heart_rate = random.choice([0, random.randint(60, 95)])
+    distance = random.randint(0, 120)
+
+    mode = calcular_modo(heart_rate, distance)
+
+    data = {
+        "device": "eloyband_01",
+        "heart_rate": heart_rate,
+        "distance_cm": distance,
+        "mode": mode,
+        "timestamp": int(time.time())
+    }
+
+    print("📤 Enviando:", data)
+    client.publish(TOPIC_DATA, json.dumps(data))
+
+    # Simulação de possível alerta
+    if mode == "Working" and random.random() < 0.05:
+        client.publish(TOPIC_ALERTS, "ALERTA: Funcionário possivelmente em overworking")
+
+    time.sleep(2)
